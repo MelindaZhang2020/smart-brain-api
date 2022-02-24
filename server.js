d60d5ae95721b279f7f1b79f1958544f195e927a
Melinda Zhang <melinda_hongye@yahoo.com>
2022-02-24 11:59:54 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const { response } = require('express');
+
+const register = require('./controllers/register')
+const signin = require('./controllers/signin')
+const profile = require('./controllers/profile')
+const image =require('./controllers/image')
+
+const knex = require('knex')({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      port : '5432',
+      user : '',
+      password : '',
+      database : 'smart-brain'
+    }
+  });
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors())
+
+app.post('/signin',(req,res) => {signin.handleSignin(req,res,knex,bcrypt)})
+app.post('/register', (req, res) => { register.handleRegister(req,res,knex,bcrypt)})
+app.get('/profile/:id', (req,res) => {profile.handleProfileGet(req,res,knex)})
+app.put('/image',(req, res) => {image.handleImage(req,res,knex)})
+app.post('/imageurl',(req, res) => {image.handleApiCall(req,res)})
+
+app.listen(3000, () => {
+    console.log('app is running on port 3000')
+})
+
+
+// --> res = this is working
+// signin --> POST = success/fail
+// register --> POST = user
+// profile/:userId --> GET = user
+// image --> PUT --> user
